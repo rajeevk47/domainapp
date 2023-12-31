3ccb37538c9571f1af5dd1abf5dc6ec175f0d12e
rajeevk47 <rajeevkumawat2007@gmail.com>
2023-12-31 23:16:34 +0530

    adds hotbarid to define id of projectile thrown from hotbar

--- public/js/eventListener.js
+++ public/js/eventListener.js
@@ -1,33 +1,6 @@
 const muteButtonwidth = 110
 let mousewheel
 let mousewheelc=0 //mousewheelcount
-addEventListener('click', (event) => {
-    
-
-    if (!players[socket.id]) { return }
-    const playerposition = {
-        x: players[socket.id].position.x + players[socket.id].width /2,
-        y: players[socket.id].position.y + players[socket.id].height/2
-    }
-    let angle =0
-    angle = Math.atan2(event.clientY - playerposition.y, event.clientX  - playerposition.x)    
-    if(event.clientX >muteButtonwidth){
-        socket.emit('shoot', {
-        x: playerposition.x,
-        y: playerposition.y,
-        angle
-    })
-    }else if(event.clientX<66&&event.clientX>24){
-        for(let i = 0;i<6;i++){
-            if(event.clientY>80+42*i && event.clientY<80 +43*(i+1)){
-                mousewheelc=i
-            }
-            
-        }
-    }
-})
-
-
 addEventListener("wheel", (event) => {
     mousewheel = event.deltaY
     if(mousewheelc==5&&mousewheel>0){
@@ -43,3 +16,33 @@ addEventListener("wheel", (event) => {
     }
     
 });
+
+addEventListener('click', (event) => {
+    
+
+    if (!players[socket.id]) { return }
+    const playerposition = {
+        x: players[socket.id].position.x + players[socket.id].width /2 ,
+        y: players[socket.id].position.y + players[socket.id].height/2
+    }
+    let angle =0
+    angle = Math.atan2(event.clientY -playerposition.y+cameray , event.clientX -playerposition.x+camerax)
+    if(event.clientX >muteButtonwidth){ 
+        socket.emit('shoot', {
+        x: canvas.width/2+camerax+ (playerdownImage.width/4)/2,
+        y:canvas.height/2+cameray+playerdownImage.height/2,
+        angle,
+        hotbarid:mousewheelc
+    })
+    }else if(event.clientX<66&&event.clientX>24){
+        for(let i = 0;i<6;i++){
+            if(event.clientY>80+42*i && event.clientY<80 +43*(i+1)){
+                mousewheelc=i
+            }
+            
+        }
+    }
+})
+
+
+
