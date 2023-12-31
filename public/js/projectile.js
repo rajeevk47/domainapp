fb6857cfaf1d4536f34fd56e50cfc84d10dc2c97
rajeevk47 <rajeevkumawat2007@gmail.com>
2023-12-31 23:19:45 +0530

    adds shootplayer id of projectile

--- public/js/projectile.js
+++ public/js/projectile.js
@@ -1,14 +1,15 @@
 class Projectile {
-    constructor({ position,image, velocity,width,height,id}) {
+    constructor({ position,image, velocity,width,height,id,shootplayerid}) {
         this.position=position
         this.image=image
         this.velocity = velocity
         this.width=width
         this.height=height
         this.id=15*id
+        this.shootplayerid=shootplayerid
     }
-    draw() {
-        c.drawImage(this.image,this.id,0,15,15,this.position.x,this.position.y,15,15)
+    draw(x,y) {
+        c.drawImage(this.image,this.id,0,15,15,this.position.x-x,this.position.y-y,15,15)
         
     }
 
