ac075df3d8986a7df6777a96d733c8b65f45007a
rajeevk47 <rajeevkumawat2007@gmail.com>
2023-12-31 23:17:58 +0530

    refac: adds hotbarpositioning retangle from index.js

--- public/js/Inventorymanager.js
+++ public/js/Inventorymanager.js
@@ -10,5 +10,9 @@ class inventory{
         for(let i=0 ; i <6; i++){
             c.drawImage(Bulletsforhotbar,this.id*i,0,25,25,35,(80+42*i),25,25)
         }
+        c.lineWidth=8
+    c.strokeStyle='red'
+    c.strokeRect(24,(72+42*mousewheelc),50,40);
+
     }
 }
